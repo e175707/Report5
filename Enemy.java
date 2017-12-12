b5af72c8b529fd402d243669b586ed1e9db9738e
e175707 <33239457+e175707@users.noreply.github.com>
2017-12-12 17:53:42 +0900

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,41 @@
+package jp.ac.uryukyu.ie.e175707;
+
+public class Enemy extends LivingThing{
+
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    @Override
+    public void attack(LivingThing opponent){
+        if(getDead()==true){
+        }else{
+            int damage = (int) (Math.random() * getAttack());
+            int fatal = (int) (Math.random() * 100);
+            if(damage == 0){
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+            }else{
+                if (fatal <= 30){
+                    damage = damage * 2;
+                    System.out.printf("%sの攻撃！痛恨の一撃！%sに%dのダメージを与えた！\n", getName(), opponent.getName(), damage);
+                    opponent.wounded(damage);
+                }else{
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた！\n", getName(), opponent.getName(), damage);
+                    opponent.wounded(damage);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void wounded(int damage){
+        int HP  = getHitPoint();
+        HP -= damage;
+        setHitPoint(HP);
+        if( HP < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+
+}
